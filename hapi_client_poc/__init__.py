7d3d54e779075e0442b86f9d20a3a432fe03c498
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2020-11-20 19:36:27 +0100

    Impl requests to get datasets info plus some refac
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- hapi_client_poc/__init__.py
+++ hapi_client_poc/__init__.py
@@ -7,15 +7,15 @@ __version__ = '0.1.0'
 import requests
 from urllib.parse import urljoin, urlparse
 from typing import Optional, Dict, List
+import logging
+
+log = logging.getLogger(__name__)
 
 
-class Dataset:
-    __slots__ = ['id', 'title', 'extra_fields']
-
-    def __init__(self, id, title=None, **kwargs):
-        self.id = id
-        self.title = title
-        self.extra_fields = kwargs
+class Endpoints:
+    CATALOG = 'catalog'
+    CAPABILITIES = 'capabilities'
+    INFO = 'info'
 
 
 def build_url(url: str, part: str) -> Optional[str]:
@@ -26,21 +26,63 @@ def build_url(url: str, part: str) -> Optional[str]:
         return None
 
 
-def get_from_endpoint(hapi_url: str, endpoint: str) -> Optional[Dict]:
+def get_from_endpoint(hapi_url: str, endpoint: str, parameters=None) -> Optional[Dict]:
     url = build_url(hapi_url, endpoint)
     if url:
-        response = requests.get(urljoin(hapi_url, 'catalog'))
+        log.debug(f"New request url:{hapi_url}, endpoint: {endpoint}, parameters:{parameters}")
+        response = requests.get(url, params=parameters)
         if response.ok:
+            log.debug(f"success!")
             response = response.json()
             if response['status']["message"] == "OK":
+                response.pop('status')
+                response.pop('HAPI')
                 return response
     else:
         raise ValueError(f"Given HAPI url seems invalid {hapi_url}")
     return None
 
 
-def get_catalog(hapi_url: str) -> Optional[List[Dataset]]:
-    response = get_from_endpoint(hapi_url, 'catalog')
+class Parameter:
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
+
+
+class DatasetInfo:
+    def __init__(self, **kwargs):
+        params = kwargs.pop('parameters')
+        self.parameters = [Parameter(**param) for param in params]
+        self.__dict__.update(kwargs)
+
+
+def get_info(hapi_url: str, parameter_id: str) -> Optional[DatasetInfo]:
+    response = get_from_endpoint(hapi_url, Endpoints.INFO, {'id': parameter_id})
     if response:
-        return [Dataset(**entry) for entry in response["catalog"]]
+        return DatasetInfo(**response)
+    return None
+
+
+class Dataset:
+
+    def __init__(self, hapi_url, **kwargs):
+        self.id = kwargs.pop('id')  # just to get completion from IDE
+        self.title = kwargs.pop('title')
+        self.__dict__.update(kwargs)
+        self._description = None
+        self._hapi_url = hapi_url
+
+    @property
+    def description(self):
+        if self._description is None:
+            self._description = get_info(self._hapi_url, self.id)
+        return self._description
+
+    def __repr__(self):
+        return f'''Dataset: id {self.id}, title {self.title}'''
+
+
+def get_catalog(hapi_url: str) -> Optional[List[Dataset]]:
+    response = get_from_endpoint(hapi_url, Endpoints.CATALOG)
+    if response:
+        return [Dataset(hapi_url, **entry) for entry in response["catalog"]]
     return None
--- tests/test_hapi_client_poc.py
+++ tests/test_hapi_client_poc.py
@@ -5,7 +5,7 @@
 import unittest
 from ddt import ddt, data, unpack
 
-from hapi_client_poc import get_catalog, get_from_endpoint
+from hapi_client_poc import get_catalog, get_info, get_from_endpoint, build_url, Endpoints
 
 
 @ddt
@@ -20,15 +20,42 @@ class TestHAPIUrlsMethods(unittest.TestCase):
         with self.assertRaises(ValueError):
             get_from_endpoint(prefix, endpoint)
 
+    def test_get_from_non_hapi_server_returns_none(self):
+        self.assertIsNone(get_from_endpoint("http://sciqlop.lpp.polytechnique.fr/", Endpoints.CAPABILITIES))
 
-class TestHAPICatalog(unittest.TestCase):
+    def test_merge_url_without_slashes(self):
+        self.assertEqual(build_url('http://server.domain', 'path'), 'http://server.domain/path')
+
+    def test_merge_url_with_leading_slash(self):
+        self.assertEqual(build_url('http://server.domain', '/path'), 'http://server.domain/path')
+
+    def test_merge_url_with_trailing_slash(self):
+        self.assertEqual(build_url('http://server.domain/', 'path'), 'http://server.domain/path')
+
+    def test_merge_url_with_both_leading_and_trailing_slashes(self):
+        self.assertEqual(build_url('http://server.domain/', 'path'), 'http://server.domain/path')
+
+
+class TestHAPIRequests(unittest.TestCase):
     def test_a_valid_server_should_provide_a_catalog(self):
         catalog = get_catalog("http://hapi.ftecs.com/hapi/")
         self.assertIsNotNone(catalog)
         self.assertGreater(len(catalog), 0)
         self.assertNotEqual(catalog[0].id, "")
         self.assertIsNotNone(catalog[0].title)
-        self.assertDictEqual(catalog[0].extra_fields,{})
 
     def test_a_wrong_server_should_return_none(self):
         self.assertIsNone(get_catalog("http://sciqlop.lpp.polytechnique.fr/"), 0)
+        self.assertIsNone(get_info("http://sciqlop.lpp.polytechnique.fr/", "some_param"), 0)
+
+    def test_a_valid_dataset_has_description(self):
+        dataset = get_catalog("http://hapi.ftecs.com/hapi/")[0]
+        self.assertIn(dataset.id, str(dataset))
+        desc = dataset.description
+        self.assertTrue(hasattr(desc, 'startDate'))
+        self.assertTrue(hasattr(desc, 'stopDate'))
+        self.assertTrue(hasattr(desc, 'parameters'))
+        param = desc.parameters[0]
+        self.assertTrue(hasattr(param, 'name'))
+        self.assertTrue(hasattr(param, 'type'))
+        self.assertTrue(hasattr(param, 'units'))
