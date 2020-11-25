b296b4953acfda6fcc98e7e6d32e3f006be4e015
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2020-11-25 15:53:00 +0100

    Started demo notebook
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- hapi_client_poc/caching/__init__.py
+++ hapi_client_poc/caching/__init__.py
@@ -1,3 +1,8 @@
+from typing import Union, Optional, List
+from datetime import datetime
+import pandas as pds
+
+
 class CachedRequest:
     def __init__(self, function):
         self.cache = {}
@@ -29,5 +34,6 @@ class DataRequestCache:
     def __init__(self, function):
         self.function = function
 
-    def __call__(self, *args, **kwargs):
-        return self.function(*args, **kwargs)
+    def __call__(self, hapi_url: str, dataset_id: str, start_time: Union[datetime, str],
+                 stop_time: Union[datetime, str], parameters: Optional[List[str]] = None) -> Optional[pds.DataFrame]:
+        return self.function(hapi_url, dataset_id, start_time, stop_time, parameters)
--- hapi_client_poc/multiprocessing/__init__.py
+++ hapi_client_poc/multiprocessing/__init__.py
@@ -5,11 +5,15 @@ Many strategies are possible here, could split requests along parameters or time
 A global process pool could be created for each client process.
 Maybe this needs to limit concurrent requests on the same server?
 """
+from typing import Union, Optional, List
+from datetime import datetime
+import pandas as pds
 
 
 class SplitDataRequest:
     def __init__(self, function):
         self.function = function
 
-    def __call__(self, *args, **kwargs):
-        return self.function(*args, **kwargs)
+    def __call__(self, hapi_url: str, dataset_id: str, start_time: Union[datetime, str],
+                 stop_time: Union[datetime, str], parameters: Optional[List[str]] = None) -> Optional[pds.DataFrame]:
+        return self.function(hapi_url, dataset_id, start_time, stop_time, parameters)
